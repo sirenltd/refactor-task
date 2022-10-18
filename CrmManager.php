9af95d4d439cf4232e36d6ce43bc25d888ffece1
einstein <einstein@deversin.com>
2022-10-19 00:43:38 +0300

    refactoring

--- CrmManager.php
+++ CrmManager.php
@@ -4,45 +4,27 @@ declare(strict_types=1);
 
 namespace App;
 
-use InvalidArgumentException;
-
 /**
  * Class CrmManager
  * @package App
  */
 class CrmManager
 {
-    private BazSender $client;
+    private SenderInterface $client;
 
-    /**
-     * @var array
-     */
-    private $settings;
-
-    public function __construct(array $settings)
+    public function __construct(SenderInterface $client)
     {
-        if (empty($settings['user'])) {
-            throw new InvalidArgumentException('User must be set!');
-        }
-
-        if (empty($settings['passwd'])) {
-            throw new InvalidArgumentException('Password must be set!');
-        }
-
-        $this->settings = $settings;
-        $this->client = new BazSender();
+        $this->client = $client;
     }
 
     /**
      * Sends the person to a crm
      *
-     * @param array $clientEntity
+     * @param ClientEntity $clientEntity
      * @return int
      */
-    public function sendPerson(array $clientEntity): int
+    public function sendPerson(ClientEntity $clientEntity): int
     {
-        $this->client->setCredentials($this->settings);
-
         return $this->client->send($clientEntity);
     }
 }
